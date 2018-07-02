9daced8a5a76bb6684b495d911f5cfe073635388
cty123 <chen2216@purdue.edu>
2018-07-02 20:20:02 +0800

    first commit

---
+++ app/src/main/java/com/example/pi/myapplication/Main2Activity.java
@@ -0,0 +1,73 @@
+package com.example.pi.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Main2Activity extends AppCompatActivity {
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private ExploreFragment exploreFragment;
+    private ProfileFragment profileFragment;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mMainFrame = findViewById(R.id.main_frame);
+        mMainNav = findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        exploreFragment = new ExploreFragment();
+        profileFragment = new ProfileFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               switch (item.getItemId()) {
+                   case R.id.nav_home:
+                       setFragment(homeFragment);
+                       return true;
+
+                   case R.id.nav_explore:
+                       setFragment(exploreFragment);
+                       return true;
+
+                   case R.id.nav_profile:
+                       setFragment(profileFragment);
+                       return true;
+
+                       default:
+                           return false;
+               }
+           }
+        });
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+}
